4305ef05e046ee124b32ed291045d8b83ec6202f
fengmk2 <fengmk2@gmail.com>
2011-06-03 10:21:06 +0800

    add exception catch

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var port = process.env.PORT || 8080;
 require('weibo').start_gtap(port, process.env.HOME);
 console.log('listen on ' + port);
+
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+});
